9b79179c8c000476c64fba3a57babea9dcb6aab5
enenkoff <enenkoff@mail.ru>
2017-06-12 01:35:42 +0300

    goodland svg gulp

--- GoodLand/gulpfile.js
+++ GoodLand/gulpfile.js
@@ -1,7 +1,35 @@
 var gulp = require('gulp'),
+    svgstore = require('gulp-svgstore'),
+    svgmin = require('gulp-svgmin'),
+    path = require('path'),
     sass = require('gulp-sass'),
     notify = require('gulp-notify'),
     rigger = require('gulp-rigger');
+
+
+
+/* create svg sprite */
+
+gulp.task('svgstore', function(){
+    gulp.src(['app/assets/images/**/*.svg','!app/assets/images/svg/logo.svg','!app/assets/images/svg/logo-gear.svg'])
+        .pipe(svgmin(function(file){
+            var prefix = path.basename(file.relative, path.extname(file.relative));
+            return {
+                plugins: [{
+                    cleanupIDs: {
+                        removeXMLProcInst:true,
+                        removeDoctype: true,
+                        removeComments: true,
+                        prefix: prefix + '_',
+                        minify: true
+                    }
+                }]
+            }
+        }))
+        .pipe(svgstore())
+        .pipe(gulp.dest('app/assets/images'))
+})
+
 
 /* compile sass to css */
 //
