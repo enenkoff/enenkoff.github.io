6a48e1a556a7b347a3c1024b445f25b3add3101f
enenkov <enenkov@zfort.com>
2017-08-04 15:28:58 +0300

    Arthrits header

--- Arthritis/app/assets/js/scripts.js
+++ Arthritis/app/assets/js/scripts.js
@@ -17,13 +17,55 @@ $(document).ready(function () {
         }
     });
 
+    /* inverting submenu*/
+
+    function invert_submenu() {
+
+        var WinWidth = $(window).width();
+        var $header_top = $('.header__top');
+        var $header_nav = $('.header__nav');
+
+        $header_top.find('nav > ul > li').each(function () {
+
+            var $this = $(this);
+            var $this_offset = $this.offset().left;
+            var $this_sub01 = $(this).find('.has-subdrop[data-sublevel="1"]');
+            var $this_sub01_off = $this_offset + 360;
+            var $this_sub02 = $(this).find('.has-subdrop[data-sublevel="2"]');
+            var $this_sub02_off = $this_offset + 540;
+
+            $this_sub01_off >= WinWidth ? $this_sub01.addClass('invert') : $this_sub01.removeClass('invert');
+            $this_sub02_off >= WinWidth ? $this_sub02.addClass('invert') : $this_sub02.removeClass('invert');
+
+        })
+
+        $header_nav.find('nav > ul > li').each(function () {
+
+            var $this = $(this);
+            var $this_offset = $this.offset().left;
+            var $this_sub01 = $(this).find('.has-subdrop[data-sublevel="1"]');
+            var $this_sub01_off = $this_offset + 480;
+            var $this_sub02 = $(this).find('.has-subdrop[data-sublevel="2"]');
+            var $this_sub02_off = $this_offset + 720;
+
+            $this_sub01_off >= WinWidth ? $this_sub01.addClass('invert') : $this_sub01.removeClass('invert');
+            $this_sub02_off >= WinWidth ? $this_sub02.addClass('invert') : $this_sub02.removeClass('invert');
+
+        })
+
+    }
+
 
 
 /* ------------------ document load ------------------ */
 
+    invert_submenu();
 
 /* ------------------ document resize ------------------ */
 
+    $(window).resize(function () {
+        invert_submenu();
+    });
 
 /* ------------------ document scroll ------------------ */
 
